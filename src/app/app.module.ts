7758b8b36e5f2c856fadcf8b0e6d11b57413862f
jordan-thirion <djodjoxyz@gmail.com>
2025-09-17 11:56:25 +0200

    ajout emailjs + modif tel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [
-  ],
+  imports: [],
+  exports: [ReactiveFormsModule],
 })
 export class AppModule { }
--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AppModule } from '../app.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
@@ -8,5 +10,37 @@ import { AppModule } from '../app.module';
   styleUrls: ['./contact.scss']
 })
 export class ContactComponent {
+contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      object: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      const templateParams = {
+        from_name: this.contactForm.value.name,
+        reply_to: this.contactForm.value.email,
+        message: this.contactForm.value.message
+      };
+
+      emailjs.send(
+        'service_8mh0275',
+        'template_qkjupum',
+        templateParams,
+        'vjYkuXp2M4_xGjndG'
+      ).then(() => {
+        alert('Message envoyé avec succès !');
+        this.contactForm.reset();
+      }).catch((error) => {
+        console.error('Erreur EmailJS:', error);
+        alert("Échec de l'envoi du message.");
+      });
+    }
+  }
 }
